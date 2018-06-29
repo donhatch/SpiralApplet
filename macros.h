dbd8fdab4d1f192bc2e423734e945b155c9034e9
Don Hatch <hatch@plunk.org>
2018-06-29 04:35:30 -0700

    sync to more modern macros.h from ShephardsPlayApplet (use CHECK instead of assert)

--- macros.h
+++ macros.h
@@ -7,15 +7,54 @@
 #define FORDOWN(i,n) for (i = (n)-1; (i) >= 0; --i) // only evaluates n once
 #define FORIDOWN(i,n) for (int i = (n)-1; (i) >= 0; --i) // only evaluates n once
 
-#define assert(expr) do { if (!(expr)) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " + #expr + ""); } while (false)
+#define CHECK(expr) do { if (!(expr)) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " + #expr + ""); } while (false)
 #define assumpt(expr) do { if (!(expr)) throw new Error("Assumption failed at "+__FILE__+"("+__LINE__+"): " + #expr + ""); } while (false)
 #define unimplemented() do {if (true) throw new Error("Unimplemented at "+__FILE__+"("+__LINE__+")"); } while (false)
 
+// NOTE: this causes a,b to be evaluated twice on failure, so is not ideal. might be better to do a block... ?
+#define CHECK_OP(a,op,b) do { if (!((a)op(b))) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): (" + #a + ")" + #op + "(" + #b + ") ("+(a)+" vs. "+(b)+")"); } while (false)
+#define CHECK_EQ(a,b) CHECK_OP(a,==,b)
+#define CHECK_LE(a,b) CHECK_OP(a,<=,b)
+#define CHECK_GE(a,b) CHECK_OP(a,>=,b)
+#define CHECK_LT(a,b) CHECK_OP(a,<,b)
+#define CHECK_GT(a,b) CHECK_OP(a,>,b)
+#define CHECK_NE(a,b) CHECK_OP(a,!=,b)  // note, not very useful if either a or b is a literal
+
+#define CHECK_OP_OP(a,op_ab,b,op_bc,c) do { if (!(((a)op_ab(b))&&((b)op_bc(c)))) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): (" + #a + ")" + #op_ab + "(" + #b + ")" + #op_bc + "(" + #c + ") ("+(a)+" vs. "+(b)+" vs. "+(c)+")"); } while (false)
+#define CHECK_LE_LE(a,b,c) CHECK_OP_OP(a,<=,b,<=,c)
+#define CHECK_LE_LT(a,b,c) CHECK_OP_OP(a,<=,b,<,c)
+#define CHECK_LT_LE(a,b,c) CHECK_OP_OP(a,<,b,<=,c)
+#define CHECK_LT_LT(a,b,c) CHECK_OP_OP(a,<,b,<,c)
+#define CHECK_NE_NE(a,b,c) CHECK_OP_OP(a,!=,b,!=,c)
+
+#define CHECK_NAN(x) do { if (!Double.isNaN(x)) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " + #x + " is "+(x)+", expected NaN"); } while (false)
+// Note: the additional "(a)==(b)||" is to make it work correctly for infinities...
+// and causes the args to be evaluated twice.
+#define CHECK_ALMOST_EQ(a,b,tol) \
+    do { \
+        if (!((a)==(b)||Math.abs((a)-(b))<=tol)) \
+            throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " \
+                           +"(" + #a + ")==(" + #b + ")+-" + #tol + "" \
+                           +" ("+(a)+" vs. "+(b)+" +- "+(tol)+")" \
+                           +" (error = "+((a)-(b))+")" \
+                           ); \
+    } while (false)
+#define CHECK_ALMOST_EQ_REL(a,b,tol) \
+    CHECK_ALMOST_EQ(a,b,(tol)*Math.max(Math.max(Math.abs(a),Math.abs(b)),1.))
+#define CHECK_ALMOST_INORDER(a,b,c,tol) \
+    do { \
+        if (!((a)-(b)<=(tol)) || !((b)-(c)<=(tol))) \
+            throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " \
+                           +"(" + #a + ")<=(" + #b + ")<=(" + #c + ")+-" + #tol + "" \
+                           +" ("+(a)+" vs. "+(b)+" vs. "+(c)+" +- "+(tol)+")" \
+                           +" (error = "+((a)-(b))+", "+((b)-(c))+")" \
+                           ); \
+    } while (false)
 
 #define INRANGE(foo,bar,baz) ((foo(bar))&&((bar)baz))
 
 #define OUT(s) System.out.println(""+s)
-#define PRINT(x) System.out.println("        " + #x + " = " + (x))
+#define PRINT(x) System.out.println(#x + " = " + (x))
 #define PRINT_(x) System.out.print(#x + " = " + (x) + ", ")
 #define PRINT2(x0,x1) {PRINT_(x0);PRINT(x1);}
 #define PRINT3(x0,x1,x2) {PRINT_(x0);PRINT2(x1,x2);}
@@ -62,9 +101,9 @@
 #define RTOD(r) ((r)*(180./Math.PI))
 #define DTOR(r) ((r)*(Math.PI/180.))
 
-#define LT(a,b,tol) (((b)-(a)) > tol)
-#define GT(a,b,tol) (((a)-(b)) > tol)
-#define LEQ(a,b,tol) (((a)-(b)) <= tol)
-#define GEQ(a,b,tol) (((b)-(a)) <= tol)
-#define EQ(a,b,tol) (LEQ(a,b,tol) && GEQ(a,b,tol))
+#define LT(a,b,eps) (((b)-(a)) > eps)
+#define GT(a,b,eps) (((a)-(b)) > eps)
+#define LEQ(a,b,eps) (((a)-(b)) <= eps)
+#define GEQ(a,b,eps) (((b)-(a)) <= eps)
+#define EQ(a,b,eps) (LEQ(a,b,eps) && GEQ(a,b,eps))
 #define MOD(a,b) (((a)+(b))%(b)) // XXX not general! only use it when a+b is known to be >= 0
